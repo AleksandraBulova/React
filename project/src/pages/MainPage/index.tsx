89884cf15cebb4a67f1542ecbdbd3d25ce5e7c89
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-03-15 16:51:29 +0300

    feat: add fillter for products

--- project/src/pages/MainPage/index.tsx
+++ project/src/pages/MainPage/index.tsx
@@ -1,15 +1,18 @@
 import ProductCard from 'components/ProductCard';
 import { products } from 'products';
 import React, { ChangeEvent } from 'react';
+import { Product } from 'types';
 import styles from './style.module.css';
 
 interface MyState {
   search: string;
+  products: Product[];
 }
 
 class MainPage extends React.Component {
   state: MyState = {
     search: '',
+    products: products,
   };
 
   componentDidMount(): void {
@@ -19,15 +22,25 @@ class MainPage extends React.Component {
     }
   }
 
-  componentDidUpdate(prevState: Readonly<MyState>): void {
+  componentDidUpdate(prevProps: MyState, prevState: MyState): void {
     if (prevState.search !== this.state.search) {
-      console.log('componentDidUpdate, search');
+      const newProducts = products.filter(
+        (product) =>
+          product.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          product.brand.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          product.type.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          product.description.toLowerCase().includes(this.state.search.toLowerCase()) ||
+          String(product.price).includes(this.state.search) ||
+          String(product.stock).includes(this.state.search)
+      );
+      this.setState((prev) => ({ ...prev, products: newProducts }));
     }
   }
 
   componentWillUnmount(): void {
     localStorage.setItem('search', this.state.search);
   }
+
   render() {
     return (
       <section className={styles.section_main_page}>
@@ -44,7 +57,7 @@ class MainPage extends React.Component {
           />
         </header>
         <div className={styles.wrapper_products}>
-          {products.map((product) => (
+          {this.state.products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
         </div>
