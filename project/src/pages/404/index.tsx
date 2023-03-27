a88a15e410e680fa28b0ede6f74f5c092cd3574b
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-03-27 17:10:38 +0300

    feat: replace Page404 class component at a functional component

--- project/src/pages/404/index.tsx
+++ project/src/pages/404/index.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styles from './style.module.css';
 
-export class Page404 extends React.Component {
-  render() {
-    return (
-      <section className={styles.wrapper}>
-        <h1 className={styles.name_error}>404</h1>
-        <div className={styles.description_error}>Page not found!</div>
-      </section>
-    );
-  }
-}
+export const Page404: FC = () => {
+  return (
+    <section className={styles.wrapper}>
+      <h1 className={styles.name_error}>404</h1>
+      <div className={styles.description_error}>Page not found!</div>
+    </section>
+  );
+};
