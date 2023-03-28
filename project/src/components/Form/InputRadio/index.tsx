e77bf22b92075d3e4f217290e9028d5526cc7283
AleksandraBulova <aliaksandra.ihnatsenia@gmail.com>
2023-03-28 10:26:09 +0300

    feat: replace InputRadio class component at a functional component

--- project/src/components/Form/InputRadio/index.tsx
+++ project/src/components/Form/InputRadio/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 import styles from './style.module.css';
 
 interface IProp {
@@ -6,30 +6,28 @@ interface IProp {
   error: boolean;
 }
 
-export class InputRadio extends React.Component<IProp> {
-  render() {
-    const strengths = ['0% - 20%', '20% - 40%', '40% - 60%'];
-    return (
-      <div className={styles.wrapper}>
-        <label className={styles.wrapper_radio}>
-          Alcohol strength:
-          {strengths.map((elem, index) => {
-            return (
-              <div key={index}>
-                <input
-                  type="radio"
-                  id={`strength${index}`}
-                  name="strength"
-                  value={elem}
-                  ref={this.props.forwardedRef}
-                />
-                <label htmlFor={`strength${index}`}>{elem}</label>
-              </div>
-            );
-          })}
-        </label>
-        {this.props.error && <div className={styles.error}>Error</div>}
-      </div>
-    );
-  }
-}
+export const InputRadio: FC<IProp> = ({ forwardedRef, error }) => {
+  const strengths = ['0% - 20%', '20% - 40%', '40% - 60%'];
+  return (
+    <div className={styles.wrapper}>
+      <label className={styles.wrapper_radio}>
+        Alcohol strength:
+        {strengths.map((elem, index) => {
+          return (
+            <div key={index}>
+              <input
+                type="radio"
+                id={`strength${index}`}
+                name="strength"
+                value={elem}
+                ref={forwardedRef}
+              />
+              <label htmlFor={`strength${index}`}>{elem}</label>
+            </div>
+          );
+        })}
+      </label>
+      {error && <div className={styles.error}>Error</div>}
+    </div>
+  );
+};
